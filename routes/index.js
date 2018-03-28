2e4d13e2c8095c9ed4ad79857fedc036a3673360
boyzhaotian <13426031783@139.com>
2018-03-28 10:37:51 +0800

    upload

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.set('X-XSS-Protection', 0) //设置浏览器不拦截xss
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', xss: req.query.xss });
 });
 
 module.exports = router;
